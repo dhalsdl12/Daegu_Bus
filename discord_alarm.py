b08bad4e00abba39585be11c13274899ec937517
dhalsdl12 <dhalsdl12@gmail.com>
2023-03-03 09:39:31 +0900

    지금 minute

--- discord_alarm.py
+++ discord_alarm.py
@@ -1,14 +1,16 @@
 import discord
 from discord.ext import commands
- 
+
+#pip install discord.py
 bot = commands.Bot(command_prefix='#', intents=discord.Intents.all())
  
 @bot.event
 async def on_ready():
     print(f'Login bot: {bot.user}')
  
-@bot.command()
-async def hello(message):
-    await message.channel.send(f'{message.author.mention}님 안녕하세요!')
+@bot.command(name='hello')
+async def inpue_message(ctx, *, message=None):
+    await ctx.channel.send(message)
+    await ctx.channel.send(f'{ctx.author.mention}님 안녕하세요!')
 
-bot.run('MTA4MDQ1NTYwMDY5OTM2MzM1OA.G3DBgW.bzx5gUzvmW1Jpv5aeIKt-weU9Xj_1QAfu5-zeQ')
+bot.run('discord bot key')
